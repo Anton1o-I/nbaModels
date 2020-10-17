f9034e0c0f8923be6fdd14784ba73c230ef5fb11
Antonio Iniguez <a.iniguez21@gmail.com>
2020-10-17 11:26:27 -0700

    added query to create dataframes for team stats and updated gitignore file '

--- data/db.py
+++ data/db.py
@@ -1,9 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import os
+import pandas as pd
+from datetime import date
 
+import data.queries as queries
+
+USER = os.environ["dbName"]
+PASSWORD = os.environ["dbPass"]
 
 class nbaDB:
-    def __init__(self, user, password):
+    def __init__(self, user: str = USER, password: str = PASSWORD):
         self.engine = create_engine(
             f"postgresql://{user}:{password}@localhost:5432/nba_stats", echo=False
         )
@@ -16,7 +23,13 @@ class nbaDB:
     def get_player_stats():
         pass
 
-    def get_game_stats():
-        pass
-
-    
+    def get_game_stats(self, start: date = None, end: date = None, team: str = None) -> pd.DataFrame:
+        res = self.session.execute(queries.get_game_results())
+        df = pd.DataFrame(res.fetchall(), columns = res.keys())
+        if start is not None:
+            df = df[df["date"] >= start]
+        if end is not None:
+            df = df[df["date"] <= end]
+        if team is not None:
+            df = df[(df["home_team"] == team) | (df["away_team"] == team) ]
+        return df
